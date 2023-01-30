07f6110f3f281ea2d57c736b61f04203369c7844
rodri-afa <rodrigo.afaformacion@gmail.com>
2023-01-30 12:41:50 +0100

    usersbyid

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -12,16 +12,19 @@ class UsuariosController extends Controller
             [
                 "nombre" => "menganito",
                 "email" => "meng@mail.com",
+                "tipo" => "normal",
                 "hobbies" => ["correr", "nadar"]
             ],
             [
                 "nombre" => "zutanito",
                 "email" => "zutanito@mail.com",
+                "tipo" => "normal",
                 "hobbies" => ["fotografía", "programar"]
             ],
             [
                 "nombre" => "maría",
                 "email" => "maria@mail.com",
+                "tipo" => "vip",
                 "hobbies" => ["videojuegos", "nadar", "crossfit"]
             ],
         ];
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Página de usuario</title>
+    <title>👋 {{$nombre}}</title>
 </head>
 <body>
-    <h1>Hola mundo</h1>
+   
+    <h1>Bienvenid@ {{$nombre}} 
+        @if ($tipo=="vip")
+            <span>👑</span>
+        @endif
+    </h1>
+
+
+    <h2>Email: {{$email}}</h2>
+    
+    <h2>Tus hobbies</h2>
+    <ul>
+        @foreach ($hobbies as $h)
+            <li>{{$h}}</li>
+        @endforeach
+
+    </ul>
 </body>
 </html>
